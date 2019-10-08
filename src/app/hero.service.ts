a40957c2e2cdfe875a81da12b3f35b4b02cccadb
Epifanio <epifaju@gmail.com>
2019-10-08 17:49:04 +0200

    suite http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class HeroService {
   
   private heroesUrl ='api/heroes'; // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
    
   constructor(
               private messageService:MessageService,
@@ -26,14 +30,22 @@ export class HeroService {
    /** GET heroes from the server */
     return this.http.get<Hero[]> (this.heroesUrl)
     .pipe(
+        tap(_=>this.log('fetched')),
         catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHeroe(id: number ):Observable<Hero> {
       // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    //this.messageService.add(`HeroService: fetched hero id=${id}`);
+    //return of(HEROES.find(hero => hero.id === id));
+
+     const url = `${this.heroesUrl}/${id}`;
+     return this.http.get<Hero>(url).pipe(
+           tap(_ => this.log(`fetched hero id=${id}`)),
+           catchError(this.handleError<Hero>(`getHero id=${id}`))
+        );
   }
 
    /** Log a HeroService message with the MessageService */
@@ -61,5 +73,13 @@ export class HeroService {
      };
      
    }
+
+   /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
 
 }
